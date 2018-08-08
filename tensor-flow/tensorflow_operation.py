7a9beb5eae934f29a5522d81bf0b3b58ae76bdb6
sai li <kellysai@126.com>
2018-08-09 03:31:49 +0800

    add matrix multiply

--- tensor-flow/tensorflow_operation.py
+++ tensor-flow/tensorflow_operation.py
@@ -15,3 +15,42 @@ with tf.Session() as sess:
 #a + b = 5
 
 
+
+#Basic Operations with variable as graph input
+#tf Graph input
+a = tf.placeholder(tf.int16)
+b = tf.placeholder(tf.int16)
+#define operations
+add = tf.add(a, b)
+multiply = tf.multiply(a, b)
+#launch the default graph
+with tf.Session() as sess:
+    #run every operation with variable input
+    print ("add result is " + str(sess.run(add, feed_dict={a:2, b:3})))
+    print ("multiply result is " + str(sess.run(multiply, feed_dict={a:5, b:4})))
+
+#add result is 5
+#multiply result is 20
+
+
+
+#Matrix MUltiplication from TensorFlow tutorial
+#Create a Constant op that produce a 1*2 matrix. The op is added as a node to the default graph
+matrix_a = tf.constant([[3,4]])
+#Create another matrix that is 2*1 matrix
+matrix_b = tf.constant([[3], [4]])
+#create a Matmul op that takes matrix_a, matrix_b as inputs.
+#the returned value, "product" represents the result of the matrix multiplication
+product = tf.matmul(matrix_a, matrix_b)
+#use session run() method to run the matul op
+with tf.Session() as sess:
+    result = sess.run(product)
+    print(result)
+
+
+#[[25]]
+
+
+
+
+
